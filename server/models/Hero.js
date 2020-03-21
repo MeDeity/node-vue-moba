2db0f6a8b39563a3fe13a46c678e58365bbd13e6
MeDeity <langrenbule@gmail.com>
2020-03-22 07:07:54 +0800

    英雄类型编辑

--- server/models/Hero.js
+++ server/models/Hero.js
@@ -3,6 +3,29 @@ const mongoos = require('mongoose')
 const schema = new mongoos.Schema({
     name: {type:String},
     avatar: {type:String},
+    title:{type:String},//称号
+    categories:[{type:mongoos.SchemaTypes.ObjectId,ref:'Category'}],//支持多个分类
+    scores:{
+        difficult:{type:Number},//难度等级
+        skills:{type:Number},//技能评分
+        attack:{type:Number},//攻击评分
+        survive:{type:Number},//生存评分
+    },
+    skills:[{
+        icon:{type:String},
+        name:{type:String},
+        description:{type:String},
+        tips:{type:String},
+    }],
+    items1:[{type:mongoos.SchemaTypes.ObjectId,ref:'Item'}],//顺风出装
+    items1:[{type:mongoos.SchemaTypes.ObjectId,ref:'Item'}],//逆风出装
+    usageTips:{type:String},//使用技巧
+    battleTips:{type:String},//对战技巧
+    teamTips:{type:String},//团战技巧
+    partners:[{
+        hero:{type:mongoos.SchemaTypes.ObjectId,ref:'Hero'},
+        description:{type:String},
+    }],
 });
 
 module.exports = mongoos.model("Hero",schema);
