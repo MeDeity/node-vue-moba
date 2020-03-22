e4b9a4fa544755311a989fd3d0fc8074927abfb0
MeDeity <langrenbule@gmail.com>
2020-03-22 21:32:38 +0800

    设置密码不可用被查询,防止被多次散列

--- server/models/AdminUser.js
+++ server/models/AdminUser.js
@@ -2,7 +2,10 @@ const mongoos = require('mongoose')
 
 const schema = new mongoos.Schema({
     username:{type:String},//用户名称
-    password:{type:String,set(val){//对密码进行散列
+    
+    password:{type:String,
+        select:false,//设置密码不会被查询出来，以防止被多次散列
+        set(val){//对密码进行散列
         return require('bcryptjs').hashSync(val,10)//bcrypt 好像过不去 所以使用了bcryptjs
     }},//密码
 });
