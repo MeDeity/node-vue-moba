2b0b229c32781ff1040a453c547a81f6a1183658
MeDeity <langrenbule@gmail.com>
2020-03-22 21:02:42 +0800

    使用 bcryptjs进行散列加密

--- server/models/AdminUser.js
+++ server/models/AdminUser.js
@@ -2,7 +2,9 @@ const mongoos = require('mongoose')
 
 const schema = new mongoos.Schema({
     username:{type:String},//用户名称
-    password:{type:String},//密码
+    password:{type:String,set(val){//对密码进行散列
+        return require('bcryptjs').hashSync(val,10)//bcrypt 好像过不去 所以使用了bcryptjs
+    }},//密码
 });
 
 module.exports = mongoos.model("AdinUser",schema);
